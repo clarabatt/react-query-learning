2910a198a2f1314a47ed82e32951d799b5d65aee
Clara Battesini <clarabattesini@gmail.com>
2022-04-15 18:24:46 -0400

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Posts } from "./Posts";
+import "./App.css";
+
+function App() {
+  return (
+    // provide React Query client to App
+    <div className="App">
+      <h1>Blog Posts</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
